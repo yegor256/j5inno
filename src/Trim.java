5168c9a4e090f2a1631c1d80dc4dc549a29e1e88
Tymur Lysenko <tymur.lysenko@gmail.com>
2021-06-03 15:33:38 +0300

    Fix trim for empty strings and non-space characters

--- src/Trim.java
+++ src/Trim.java
@@ -4,14 +4,6 @@ class Trim {
         this.text = t;
     }
     public String take() {
-        int start = 0;
-        while (this.text.charAt(start) == ' ') {
-            start += 1;
-        }
-        int end = this.text.length() - 1;
-        while (this.text.charAt(end) == ' ') {
-            end -= 1;
-        }
-        return this.text.substring(start, end + 1);
+        return text.trim();
     }
 }
